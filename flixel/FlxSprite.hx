614b624d42e4c7d834e7462e8bddac0bf38abece
NeeEoo <NeeEoo@users.noreply.github.com>
2023-06-11 14:48:39 +0200

    Fixed bug with camera shaders on sprites

--- flixel/FlxSprite.hx
+++ flixel/FlxSprite.hx
@@ -836,6 +836,9 @@ class FlxSprite extends FlxObject
 		if (dirty) // rarely
 			calcFrame(useFramePixels);
 
+		if (shader != null && shader is FlxGraphicsShader)
+			shader.setCamSize(_frame.frame.x, _frame.frame.y, _frame.frame.width, _frame.frame.height);
+
 		for (camera in cameras)
 		{
 			if (!camera.visible || !camera.exists || !isOnScreen(camera))

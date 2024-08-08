3cbea32ae7d34e7413e89b0c9106d1adfdd369a1
Ne_Eo <NeeEoo@users.noreply.github.com>
2024-08-08 14:56:11 +0200

    Small Ram optimizations

--- flixel/FlxBasic.hx
+++ flixel/FlxBasic.hx
@@ -1,5 +1,6 @@
 package flixel;
 
+import flixel.FlxTypes;
 import flixel.util.FlxDestroyUtil.IFlxDestroyable;
 import flixel.util.FlxStringUtil;
 
@@ -198,7 +199,7 @@ class FlxBasic implements IFlxDestroyable
 /**
  * Types of flixel objects - mainly for collisions.
  */
-enum abstract FlxType(Int)
+enum abstract FlxType(ByteInt)
 {
 	var NONE = 0;
 	var OBJECT = 1;
---
+++ flixel/FlxTypes.hx
@@ -0,0 +1,6 @@
+package flixel;
+
+typedef ByteInt = #if cpp cpp.Int8 #else Int #end;
+typedef ByteUInt = #if cpp cpp.UInt8 #else UInt #end;
+typedef ShortInt = #if cpp cpp.Int16 #else Int #end;
+typedef ShortUInt = #if cpp cpp.UInt16 #else UInt #end;
